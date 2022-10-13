79031e60caafdd29938f13a90f7a1bb365b81b21
lulusansleo <luan.bourbier@epitech.eu>
2022-10-13 14:41:46 +0200

    second_file.c

--- second_file.c
+++ second_file.c
@@ -11,8 +11,6 @@ int count_island(char **world)
 {
     vector_t size;
 
-    // if (!world || check_format(world))
-    //     return 84;
     size = size_of_world(world);
     return actual_count(size, world);
 
